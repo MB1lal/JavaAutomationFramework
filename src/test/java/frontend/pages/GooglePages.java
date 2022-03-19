da068c7a1ddde20901f573978dcd178ad1b22db0
Bilal <m.b1lal@hotmail.com>
2022-03-19 22:10:15 +0400

    Added chromeOptions in serenity.properties

--- src/test/java/frontend/pages/GooglePages.java
+++ src/test/java/frontend/pages/GooglePages.java
@@ -4,7 +4,6 @@ import net.serenitybdd.core.pages.WebElementFacade;
 import net.thucydides.core.annotations.DefaultUrl;
 import net.thucydides.core.pages.PageObject;
 import org.openqa.selenium.support.FindBy;
-
 import java.time.Duration;
 
 @DefaultUrl("page:googleURL")
