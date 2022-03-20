5b744607675f439b67af7ff7cf6c7dfb68c355f0
Bilal <m.b1lal@hotmail.com>
2022-03-20 17:21:22 +0400

    Removed redundant declerations

--- src/test/java/frontend/steps/BaseSteps.java
+++ src/test/java/frontend/steps/BaseSteps.java
@@ -1,15 +1,9 @@
 package frontend.steps;
 
-import core.Env;
-import frontend.ui.Browser;
+
 import utils.ExcelReader;
-import utils.SharedState;
 
 public class BaseSteps {
-
-    public SharedState sharedState = new SharedState();
-    public Env env = new Env();
-    public Browser browser = new Browser();
     public utils.ExcelReader excelReader = utils.ExcelReader.getInstance();
 
 }
--- src/test/java/frontend/steps/Hooks.java
+++ src/test/java/frontend/steps/Hooks.java
@@ -1,36 +1,12 @@
 package frontend.steps;
 
-import core.EnvSerenity;
-import frontend.ui.models.BrowserType;
-import io.cucumber.java.After;
 import io.cucumber.java.Before;
 
 import io.cucumber.java.Scenario;
 import net.serenitybdd.core.Serenity;
-
 import static utils.SharedStateConstants.FRONTEND.EXCEL_DATA;
-import static utils.SharedStateConstants.FRONTEND.WEBDRIVER;
 
 public class Hooks extends BaseSteps{
-
-    @Before(value = "@Browser", order = 0)
-    public void setupBrowser() {
-        if(EnvSerenity.webDriver.equalsIgnoreCase("chrome"))
-        {
-            env.setDriver(BrowserType.CHROME);
-        }
-    }
-
-    @Before(value = "@Browser", order = 1)
-    public void startBrowser() throws Exception {
-        sharedState.setWebDriver(browser.getChromeDriver(env.getDriver()));
-        Serenity.setSessionVariable(WEBDRIVER).to(sharedState.getWebDriver());
-    }
-
-    @After(value = "@Browser", order = 0)
-    public void closeBrowser() {
-        sharedState.getWebDriver().quit();
-    }
 
     @Before
     public void bootstrap(Scenario scenario) {
