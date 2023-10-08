930bf56188b08e219921cca80e5f9ed74120996e
Bilal <m.b1lal@hotmail.com>
2023-10-08 21:23:17 +0500

    Fixed Dropdown failure

--- src/test/java/pages/DropdownPage.java
+++ src/test/java/pages/DropdownPage.java
@@ -1,25 +1,31 @@
 package pages;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.FindBy;
 
 public class DropdownPage extends PageObject {
 
     @FindBy(id = "dropdown")
-    private WebElement dropdownElement;
+    private WebElementFacade dropdownElement;
 
-    private final Select dropdown;
+    private Select dropdown;
 
-    public DropdownPage() {
+    private void initialiseDropdown() {
         dropdown = new Select(dropdownElement);
     }
 
     public void selectDropdownOption(String option) {
+        initialiseDropdown();
         dropdown.selectByVisibleText(option);
     }
 
-    public String getDropdownSelectedOption() { return dropdown.getFirstSelectedOption().toString(); }
+    public String getDropdownSelectedOption() {
+        initialiseDropdown();
+        return dropdown.getFirstSelectedOption().getText();
+    }
 
 }
